9657bfa1f8c192a7cc1c7fd455773e48788a43a1
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-04-28 22:16:59 +0300

    -->

--- analysis.py
+++ analysis.py
@@ -33,9 +33,44 @@ df[['Purchase Date','Purchase Year','Purchase Month']]
 pirkimai_pagal_lyti = df.groupby('Gender')['Total Purchase Amount'].mean()
 # print(pirkimai_pagal_lyti)
 
-mokejimo_metodas_pagal_lyti = df.groupby('Gender')['Payment Method'].value_counts()
+# mokejimo_metodas_pagal_lyti = df.groupby('Gender')['Payment Method'].value_counts()
 # print(mokejimo_metodas_pagal_lyti)
 
-sns.countplot(data=df, order=mokejimo_metodas_pagal_lyti.index,)
-plt.legend()
+# sns.countplot(data=df, order=mokejimo_metodas_pagal_lyti.index)
+# plt.legend()
+# plt.show()
+
+#hue - atskiria papildomus duomenu aspektus
+sns.countplot(data=df, x='Payment Method', hue='Gender', order=df['Payment Method'].value_counts().index, palette='husl')
+plt.legend(title='Lytis')
+plt.show()
+
+
+#klientu amziaus kategorijos
+jauniausias_klientas = df['Age'].min()
+print(f'Jauniausias klientas: {jauniausias_klientas}')
+
+amzius_iki_25 = df[df['Age'] < 25].shape[0]
+print(f'Klientai jaunesni nei 25 m.: {amzius_iki_25}')
+
+klientai_nuo_25_iki_50 = df[(df['Age'] > 25) & (df['Age'] < 50)].shape[0]
+print(f'Klientai nuo 25 m. iki 50 m.: {klientai_nuo_25_iki_50}')
+
+vyriausias_klientas = df['Age'].max()
+print(f'Vyriausias klientas: {vyriausias_klientas}')
+
+amzius_nuo_50 = df[(df['Age'] > 50)].shape[0]
+print(f'Klientai nuo 50 m.: {amzius_nuo_50}')
+
+Age_groups = ['Customers under 25 years of age', 'Customers aged 25 to 50', 'Customers aged 50+']
+Quantity = [amzius_iki_25, klientai_nuo_25_iki_50, amzius_nuo_50]
+
+df = pd.DataFrame({'Age groups': Age_groups, 'Quantity': Quantity})
+
+plt.figure(figsize=(10, 8))
+sns.barplot(data=df, x='Age groups', y='Quantity', hue='Age groups', palette='husl')
+plt.xlabel('Age groups', fontsize=13)
+plt.ylabel('Quantity of customers', fontsize=13)
+# plt.xticks(rotation=7)
+plt.title('Number of customers by age group', fontsize=15)
 plt.show()
