410c9e0ee61f675522bc5bcc4f0de75e86359fa6
Samanta <163418549+Samantjna@users.noreply.github.com>
2024-04-29 16:30:02 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ import seaborn as sns
 #Duomenu valymas ir paruosimas naudojimui
 df = pd.read_csv('ecommerce_customer_data_custom_ratios.csv')
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 # print(df.describe())
 
 #Tikriname eiluciu skaiciu
@@ -33,38 +34,58 @@ df[['Purchase Date','Purchase Year','Purchase Month']]
 pirkimai_pagal_lyti = df.groupby('Gender')['Total Purchase Amount'].mean()
 # print(pirkimai_pagal_lyti)
 
+# mokejimo_metodas_pagal_lyti = df.groupby('Gender')['Payment Method'].value_counts()
+# print(mokejimo_metodas_pagal_lyti)
 
-#mokejimo metodai pagal lyti
-sns.countplot(data=df, x='Payment Method', hue='Gender', order=df['Payment Method'].value_counts().index, palette='husl')       #hue - atskiria papildomus duomenu aspektus
-plt.legend(title='Lytis')
-plt.show()
+# sns.countplot(data=df, order=mokejimo_metodas_pagal_lyti.index)
+# plt.legend()
+# plt.show()
 
 
 #klientu amziaus grupes
-jauniausias_klientas = df['Age'].min()
-print(f'Jauniausias klientas: {jauniausias_klientas}')
 
+jauniausias_klientas = df['Age'].min()
+# print(f'Jauniausias klientas: {jauniausias_klientas}')
+                                                         # suzinome tiek vyriausio, tiek jauniausio kliento amziu.
+vyriausias_klientas = df['Age'].max()
+# print(f'Vyriausias klientas: {vyriausias_klientas}')
+
+
+df['amzius_iki_25'] = df[df['Age'] < 25].shape[0]
+# print(f'Klientai jaunesni nei 25 m.: {amzius_iki_25}')
+
+df['klientai_nuo_25_iki_50'] = df[(df['Age'] > 25) & (df['Age'] < 50)].shape[0]
+# print(f'Klientai nuo 25 m. iki 50 m.: {klientai_nuo_25_iki_50}')
+
+df['amzius_nuo_50'] = df[(df['Age'] > 50)].shape[0]
+# print(f'Klientai nuo 50 m.: {amzius_nuo_50}')
+
+#----------------------------------------------------------------
 amzius_iki_25 = df[df['Age'] < 25].shape[0]
-print(f'Klientai jaunesni nei 25 m.: {amzius_iki_25}')
 
 klientai_nuo_25_iki_50 = df[(df['Age'] > 25) & (df['Age'] < 50)].shape[0]
-print(f'Klientai nuo 25 m. iki 50 m.: {klientai_nuo_25_iki_50}')
-
-vyriausias_klientas = df['Age'].max()
-print(f'Vyriausias klientas: {vyriausias_klientas}')
 
 amzius_nuo_50 = df[(df['Age'] > 50)].shape[0]
-print(f'Klientai nuo 50 m.: {amzius_nuo_50}')
 
 age_groups = ['Customers under 25 years of age', 'Customers aged 25 to 50', 'Customers aged 50+']
 quantity = [amzius_iki_25, klientai_nuo_25_iki_50, amzius_nuo_50]
 
-df = pd.DataFrame({'Age groups': age_groups, 'Quantity': quantity})
+features = pd.DataFrame({'Age groups': age_groups, 'Quantity': quantity})
 
 plt.figure(figsize=(10, 8))
-sns.barplot(data=df, x='Age groups', y='Quantity', hue='Age groups', palette='husl')
+sns.barplot(data=features, x='Age groups', y='Quantity', hue='Age groups', palette='husl')
 plt.xlabel('Age groups', fontsize=13)
 plt.ylabel('Quantity of customers', fontsize=13)
 # plt.xticks(rotation=7)
 plt.title('Number of customers by age group', fontsize=15)
-plt.show()
+# plt.show()
+
+#----------------------------------------------------------------
+
+# pagal klientu amziu ir lyti, bendros sumos isleistu pinigu pasiskirstymas
+
+pirkimai_pagal_lyti_ir_amziu = df.groupby(['Gender', 'Age'])['Total Purchase Amount'].sum()
+# print(pirkimai_pagal_lyti_ir_amziu)
+
+amziaus_grupe_pagal_pirkima = df.groupby(['klientai_nuo_25_iki_50', 'amzius_nuo_50', 'amzius_iki_25'])['Total Purchase Amount'].mean()
+print(amziaus_grupe_pagal_pirkima)
