115d897fe5985eba4ba40e58845d8e8cc8f39332
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-05-02 11:23:52 +0300

    -->

--- gaal_veikiantis_modeliukas.py
+++ gaal_veikiantis_modeliukas.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, GRU, Dense
 from sklearn.model_selection import train_test_split
 from datetime import datetime
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 df = pd.read_csv('ecommerce_customer_data_custom_ratios.csv')
@@ -22,7 +23,6 @@ df_selected['Year'] = df_selected['Purchase Date'].dt.year
 df_selected['Month'] = df_selected['Purchase Date'].dt.month
 df_selected['Day'] = df_selected['Purchase Date'].dt.day
 df_selected['Hour'] = df_selected['Purchase Date'].dt.hour
-
 # print(df_selected.head())
 
 # Sumuojami pirkimai pagal metus ir mėnesius
@@ -47,14 +47,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_
 
 # LSTM modelis
 model = Sequential([
-    LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], 1)),
-    LSTM(50),
+    GRU(units=133, return_sequences=True, input_shape=(X_train.shape[1], 1)),
+    GRU(units=133),
     Dense(1)
 ])
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='rmsprop', loss='mean_squared_error')
 
 # Treniravimas
-model.fit(X_train, Y_train, epochs=50, batch_size=32, validation_data=(X_test, Y_test), verbose=1)
+model.fit(X_train, Y_train, epochs=33, batch_size=16, validation_data=(X_test, Y_test), verbose=1)
 
 # Prognozė
 predictions = model.predict(X_test)
@@ -62,6 +62,13 @@ predictions = model.predict(X_test)
 # Atstatomas orginalus mastelis
 predictions = scaler.inverse_transform(predictions)
 Y_test = scaler.inverse_transform(Y_test.reshape(-1, 1))
+
+rmse = np.sqrt(mean_squared_error(Y_test, predictions))
+print("RMSE:", rmse)
+
+# Įvertinimas naudojant R^2
+r2 = r2_score(Y_test, predictions)
+print("R2:", r2)
 
 # Atvaizduoti rezultatus
 plt.plot(Y_test, label='Pagal faktą')
@@ -72,4 +79,16 @@ plt.title('Faktiniai ir prognozuojami pirkimai')
 plt.legend()
 plt.show()
 
+# Geriausi hiperparametrai: {'batch_size': 16, 'epochs': 11, 'lstm_units': 133, 'optimizer': 'rmsprop'}
+# RMSE: 0.06881850797856753
+# R2: -2.086435104145237
+
+#SU LSTM ir 33 epochom
+# RMSE: 385.84462729521374
+# R2: 0.022753505179238354
+
+
+#SU GRU ir 33 epochom
+# RMSE: 380.98184157275733
+# R2: 0.04723068818549825
 
---
+++ hiperparametrai.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from scikeras.wrappers import KerasRegressor
+from scipy.stats import randint as sp_randint
+
+
+df = pd.read_csv('ecommerce_customer_data_custom_ratios.csv')
+selected_features = ['Product Category', 'Product Price', 'Quantity', 'Purchase Date']
+df_selected = df[selected_features]
+
+# Konvertuojama į datetime formatą
+df_selected['Purchase Date'] = pd.to_datetime(df_selected['Purchase Date'])
+
+df_selected['Year'] = df_selected['Purchase Date'].dt.year
+df_selected['Month'] = df_selected['Purchase Date'].dt.month
+df_selected['Day'] = df_selected['Purchase Date'].dt.day
+df_selected['Hour'] = df_selected['Purchase Date'].dt.hour
+
+# Sumuojami pirkimai pagal metus ir mėnesius
+df_grouped = df_selected.groupby(['Year', 'Month']).agg({'Quantity': 'sum'}).reset_index()
+
+# Duomenys ruošiami modeliui
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(df_grouped['Quantity'].values.reshape(-1, 1))
+
+# Mokymo ir testavimo rinkiniai
+look_back = 12
+X, Y = [], []
+for i in range(look_back, len(scaled_data)):
+    X.append(scaled_data[i - look_back:i, 0])
+    Y.append(scaled_data[i, 0])
+
+X, Y = np.array(X), np.array(Y)
+X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+# LSTM modelis
+def create_model(lstm_units=50, optimizer='adam'):
+    model = Sequential([
+        LSTM(lstm_units, return_sequences=True, input_shape=(X_train.shape[1], 1)),
+        LSTM(lstm_units),
+        Dense(1)
+    ])
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    return model
+
+param = {
+    'optimizer': ['adam', 'rmsprop'],
+    'batch_size': [16, 32, 64, 128],
+    'epochs': sp_randint(10, 100),
+    'lstm_units': sp_randint(50, 200)
+}
+
+# Inicijuojama KerasRegressor
+keras_model = KerasRegressor(build_fn=create_model, lstm_units=50, verbose=0)
+
+# Atsitiktinė paieška
+# cv5 kartus mokomas, n iter atsitiktiniu deriniu bandymai
+random_search = RandomizedSearchCV(estimator=keras_model, param_distributions=param,
+                                   n_iter=20, cv=5, verbose=2, random_state=42)
+
+# Paleidžiama paieška
+random_search.fit(X_train, Y_train)
+
+# Geriausi parametrai
+print("Geriausi hiperparametrai:", random_search.best_params_)
+
+# Geriausi parametrai
+best_model = random_search.best_estimator_
+predictions = best_model.predict(X_test)
+rmse = np.sqrt(np.mean((predictions - Y_test) ** 2))
+r2 = best_model.score(X_test, Y_test)
+print("RMSE:", rmse)
+print("R2:", r2)
